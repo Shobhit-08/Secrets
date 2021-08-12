77fbd0682ab227c68a2e430afc4991e4530a93d4
Shobhit-08 <kumarshobhit65@yahoo.com>
2021-08-12 06:03:07 +0530

    Level 2: using Encryption and secret string

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express=require("express");
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const ejs=require('ejs');
+const encrypt=require('mongoose-encryption');
 
 const app=express();
 app.set('view engine','ejs');
@@ -14,6 +15,11 @@ const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
+
+const secret="Thisisthesecretformongooseencryptiontesting";
+
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+
 const User=mongoose.model('User',userSchema);
 
 app.get('/',(req,res)=>{
